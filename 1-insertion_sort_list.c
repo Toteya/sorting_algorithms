d48740625bfbd50f7b8bb785ce1712dd2cb938e6
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-20 21:32:03 +0200

    Task1: Fixed faulty code - swapping elements subroutine

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,28 +8,31 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *position, *curr, *prev = NULL;
 
+	if (!list)
+		return;
+
 	position = *list;
 
 	while(position != NULL)
 	{
 		curr = position;
+		prev = curr->prev;
 		while(prev && curr->n < prev->n)
 		{
 			prev->next = curr->next;
 			curr->prev = prev->prev;
-			curr->next = prev;
-			prev->prev = curr;
-			
 			if(prev->prev)
 				(prev->prev)->next = curr;
 			if(curr->next)
 				(curr->next)->prev = prev;
-
-			curr = curr->prev;
-			if(curr->prev)
-				prev = curr->prev;
-			else
+			curr->next = prev;
+			prev->prev = curr;
+			
+			prev = curr->prev;
+			if (!prev)
 				*list = curr;
+
+			print_list(*list);
 		}
 
 		position = position->next;
