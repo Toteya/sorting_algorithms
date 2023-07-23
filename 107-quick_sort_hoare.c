4ae917752b534eb489bf426afdb143972b3c6ce6
Toteya <111172685+Toteya@users.noreply.github.com>
2023-07-24 01:07:17 +0200

    Quick_sort Hoare, fixed seg fault, 2-element bug, betty

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
-void sort_partition(int *array, size_t start, size_t end);
+void sort_partition(int *array, size_t start, size_t end, size_t size);
 
 /**
- * quick_sort_hoare: sorts an array in ascending order using 
+ * quick_sort_hoare - sorts an array in ascending order using
  * the quick_sort hoare algorithm
  * @array: the array to be sorted
  * @size: size of the array
@@ -20,18 +20,19 @@ void quick_sort_hoare(int *array, size_t size)
 	start = 0;
 	end = size - 1;
 
-	sort_partition(array, start, end);
+	sort_partition(array, start, end, size);
 }
 
 /**
- * sort_partition: sorts the partition of the array
+ * sort_partition - sorts the partition of the array
  * @array: array containing the partition
+ * @size: size of the array
  * @start: start of the partition
  * @end: end of parttion
  * Return: Nothing
  */
 
-void sort_partition(int *array, size_t start, size_t end)
+void sort_partition(int *array, size_t start, size_t end, size_t size)
 {
 	size_t pivot, i, j;
 	int tmp;
@@ -40,7 +41,7 @@ void sort_partition(int *array, size_t start, size_t end)
 	i = start;
 	j = end - 1;
 
-	while (j > i)
+	while (j >= i)
 	{
 		while (array[i] <= array[pivot] && i < pivot)
 			i++;
@@ -53,6 +54,7 @@ void sort_partition(int *array, size_t start, size_t end)
 		tmp = array[i];
 		array[i] = array[j];
 		array[j] = tmp;
+		print_array(array, size);
 	}
 
 	if (i != pivot)
@@ -60,10 +62,11 @@ void sort_partition(int *array, size_t start, size_t end)
 		tmp = array[i];
 		array[i] = array[pivot];
 		array[pivot] = tmp;
+		print_array(array, size);
 	}
 
 	if ((i - start) > 1)
-		sort_partition(array, start, i - 1);
+		sort_partition(array, start, i - 1, size);
 	if ((end - i) > 1)
-		sort_partition(array, i + 1, end);
+		sort_partition(array, i + 1, end, size);
 }
